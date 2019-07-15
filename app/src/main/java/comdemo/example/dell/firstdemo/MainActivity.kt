af605c60190ca570c849024a068f338c3f45fe4f
767200412 <51736946+767200412@users.noreply.github.com>
2019-07-15 18:00:22 +0800

    Study ConstraintLayout

---
+++ app/src/main/java/comdemo/example/dell/firstdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package comdemo.example.dell.firstdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_two)
+    }
+}
